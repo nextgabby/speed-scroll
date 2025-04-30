4687ff7a823390db8ab1be8f0be934da6723f200
Gabby Yuhas <gyuhas@twitter.com>
2025-04-30 16:29:08 -0400

    new

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get("/webhook", (req, res) => {
 });
 
 
-app.get("/", (req, res) => res.send("app is running"));
+app.get("/", (req, res) => res.send("speed scroll app is running"));
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`App listening on port: ${PORT}`));
